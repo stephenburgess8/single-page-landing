fb4359c6ae73694f80f1c490db939a30b34c6cc7
Stephen Burgess <sburgess@shineunited.com>
2015-11-04 09:39:24 -0600

    Initial import

---
+++ js/scripts.js
@@ -0,0 +1,83 @@
+window.onload = function()
+{
+	menuHandler();
+	animations();
+}
+
+function menuHandler()
+{
+	var menuIconContainer = document.getElementById("menu-icon-container"),
+		navList = document.getElementsByClassName("nav-ul")[0],
+		navItems = document.getElementsByClassName("nav-li"),
+		navItemsLength = navItems.length;
+	
+	menuIconContainer.addEventListener('click', function(e)
+	{
+		openCloseMenu();
+	}, false);
+
+	function openCloseMenu()
+	{
+		if(navList.className === "nav-ul")
+		{
+			navList.style.display = "block";
+			for(var i = 0; i < navItemsLength; i++)
+			{
+				navItems[i].style.display =  "block";
+			}
+			navList.className = "nav-ul open";
+		}
+		else
+		{
+			navList.style.display = "";
+			for(var i = 0; i < navItemsLength; i++)
+			{
+				navItems[i].style.display =  "";
+			}
+			navList.className = "nav-ul";
+		}
+	}
+}
+
+function animations()
+{
+	var productFeatureBlur = document.getElementById('product-feature-blur');
+	CSSPlugin.defaultForce3D = false;
+	
+	new Waypoint({
+		element: productFeatureBlur,
+		handler: function(direction)
+		{
+			if (direction === "down")
+			{
+				TweenLite.to(productFeatureBlur, 2, {
+					css: {'opacity': '0'},
+					ease: Power1.easeOut
+				});
+			}
+			else
+			{
+				TweenLite.to(productFeatureBlur, 2, {
+					css: {'opacity': '1'},
+					ease: Power1.easeOut
+				});
+			}
+		},
+		offset: 600
+	});
+
+	new Waypoint({
+		element: productFeatureBlur,
+		handler: function(direction)
+		{
+			if (direction === "down")
+			{
+				TweenLite.to(productFeatureBlur, 2, {
+					css: {'opacity': '1'},
+					ease: Power1.easeOut
+				});
+			}
+		},
+		offset: 700
+	});
+}
